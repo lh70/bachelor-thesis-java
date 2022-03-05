3a2e5493b0e56e6d48a921f2cd4a96985311d27a
Lukas Holst <lukas@familie-holst.de>
2022-03-05 03:17:44 +0100

    #noissue removed DataInputStream and DataOutputStream from Connection.

--- src/main/scala/Connection.scala
+++ src/main/scala/Connection.scala
@@ -1,13 +1,12 @@
-import java.io.{DataInputStream, DataOutputStream, IOException, OutputStream}
+import java.io.{IOException, InputStream, OutputStream}
 import java.net.Socket
 import java.nio.charset.StandardCharsets
 import java.nio.{ByteBuffer, ByteOrder}
 
 class Connection(socket: Socket) {
+  private val in = new InputStreamBuffer(socket.getInputStream)
+  private val out = socket.getOutputStream
 
-
-  // alternative methods to read/write ints and bytes
-  // byte buffer is a newer api (java.nio) than DataInput/OutputStream ... but its never clear if that is good or bad?
   def toBytes(i: Int): Array[Byte] = ByteBuffer.allocate(Integer.BYTES).order(ByteOrder.BIG_ENDIAN).putInt(i).array()
   def toInt(array: Array[Byte]): Int = ByteBuffer.wrap(array).order(ByteOrder.BIG_ENDIAN).getInt
 
@@ -16,28 +15,48 @@ class Connection(socket: Socket) {
     // message must be utf-8 encoded
     val encodedMessage = message.getBytes(StandardCharsets.UTF_8)
     // length cannot exceed signed int length, so max-unsigned-int is not checked atm
-    val length = encodedMessage.length
-    val out = socket.getOutputStream
-    DataOutputStream(out).writeInt(length)
+    out.write(toBytes(encodedMessage.length))
     out.write(encodedMessage)
     out.flush()
   }
 
   /** Receives a message with correct encoding and so on */
   def receive(): String = {
-    // DataInputStream has some convenience methods
-    val in = new DataInputStream(socket.getInputStream)
     // preamble is 4 bytes long
-
-    val length = in.readInt()
+    in.readNBytes(4)
+    val length = toInt(in.buffer.slice(0, 4))
     // cannot allocate more than Integer.MAX_VALUE array space
     // this is only half of what a message can theoretically be
     if (Integer.compareUnsigned(length, Integer.MAX_VALUE) > 0) { // cannot handle this atm
       throw new IOException("Message is longer than " + Integer.MAX_VALUE + " bytes, which is Java array maximum.")
     }
     // message is utf-8 encoded
-    new String(in.readNBytes(length), StandardCharsets.UTF_8)
+    in.readNBytes(length)
+    new String(in.buffer.slice(0, length), StandardCharsets.UTF_8)
   }
 
   def close(): Unit = socket.close()
 }
+
+class InputStreamBuffer(val in: InputStream) {
+  var buffer = new Array[Byte](1)
+
+  def readNBytes(length: Int): Unit = {
+    if (buffer.length < length) {
+      buffer = new Array[Byte](length)
+    }
+
+    var totalBytesRead = 0
+    var currentBytesRead = 0
+    while (totalBytesRead < length) {
+      currentBytesRead = in.read(buffer, totalBytesRead, length-totalBytesRead)
+
+      if (currentBytesRead == -1) {
+        throw new Exception("socket closed")
+      }
+
+      totalBytesRead = totalBytesRead + currentBytesRead
+    }
+  }
+}
+
